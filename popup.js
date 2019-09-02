a80ca693f9927ab5069366def160097cf0bf1b62
mariyan96 <mariyan_dimitrov@yahoo.com>
2019-09-02 18:07:47 +0300

    Added "Just do it" meme on "Mark Complete" button

--- popup.js
+++ popup.js
@@ -2,11 +2,13 @@
 var dankExtention = {
     memeLength: {
         'mission-complete': 8000,
+        'do-it': 1400
     },
     memeStateFor: {
         'mission-complete': false,
+        'do-it': false
     },
-    triggerMeme: function ( memeName ) {
+    triggerMeme: function ( memeName, pictureFormat ) {
         let that = this;
         // simple check so that sound are not overlappping
         if( that.memeStateFor[ memeName ] ) {
@@ -14,39 +16,57 @@ var dankExtention = {
         }
 
         // seatching the URL's from the extention to the page's context 
-        let musicURL = chrome.extension.getURL('/resources/' + memeName + '/music.mp3');
-        let pictureURL = chrome.extension.getURL('/resources/' + memeName + '/picture.png')
+        let musicURL = chrome.extension.getURL('/resources/' + memeName + '/music.mp3'  );
+        let pictureURL = chrome.extension.getURL('/resources/' + memeName + '/picture.' + pictureFormat )
         let music = new Audio(musicURL);
 
         that.memeStateFor[memeName]= true;
 
         //reusing the modal
-        $( '#' + memeName ).remove();
+        $( '.' + memeName ).remove();
         $( 'body' ).append(
-            `<div id="${ memeName }">
+            `<div class="meme-land ${ memeName }">
                 <div class="wav"><img src="${ pictureURL }" alt="" /></div>
             </div>`
         );
 
-        $( '#' + memeName ).addClass( 'is-visible' );
+        // showing the modal
+        $( '.' + memeName ).addClass( 'is-visible' );
         music.play();
-    
+        
+        // hiding the modal when the music/audio stops
         setTimeout( function () {
-            $( '#' + memeName ).removeClass( 'is-visible' );
+            $( '.' + memeName ).removeClass( 'is-visible' );
             music.pause();
             music.currentTime = 0;
             that.memeStateFor[ memeName ] = false;
         }, that.memeLength[ memeName ] );
 
     },
+    // the attaching of events
     init: function () {
         let that = this;
         $( document ).on( 'click' , '.Button.Button--small.Button--secondary.CompletionButton--isIncomplete.CompletionButton, .TaskRowCompletionStatus-checkbox.TaskRowCompletionStatus-checkbox--incomplete' , function () {
-            that.triggerMeme( 'mission-complete' );
+            that.triggerMeme( 'mission-complete', 'png' );
         });
+
+        let hoverMemeTimeOut = {};
+
+        // delegate events for hover-in and hover-out
+        $('body').on({
+            mouseenter: function() {
+                hoverMemeTimeOut = setTimeout(function () {
+                    that.triggerMeme( 'do-it', 'gif' );
+                }, 2000);
+            },
+            mouseleave: function() {
+                clearTimeout(hoverMemeTimeOut);
+            }
+        }, '.Button.Button--small.Button--secondary.CompletionButton--isIncomplete.CompletionButton');
     }
 };
 
+// initializing the script only once
 if ( ! window.dankExtInitialized ) {
     window.dankExtInitialized = true;
     dankExtention.init();
